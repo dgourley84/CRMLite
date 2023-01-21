208669db08a60217f207da7a83d67fad0cefd3a0
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2023-01-21 15:57:30 +1000

    Styling and set up of Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { themeSettings } from "theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout"
 
 
 function App() {
@@ -10,9 +13,17 @@ function App() {
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route element = {<Layout />}>
+              <Route path="/" element={<Navigate to ="/dashboard" replace/>}/>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
