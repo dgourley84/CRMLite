5bedc140e9cb46c8ac27a0f176e252427989a3cd
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2023-01-31 23:18:52 +1000

    amend the JWT and password

--- server/utils/authMiddleWare.js
+++ server/utils/authMiddleWare.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
-import ENV from '../utils/jwtConfig.js';
+
 
 /** auth middleware */
 export default async function Auth(req, res, next) {
