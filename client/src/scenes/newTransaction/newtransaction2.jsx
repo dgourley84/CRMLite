f4f993aecd398b07c8aef448390ded6a4e891537
Tyson <tyson@alittledesigner.com>
2023-02-04 14:58:19 +0800

    new transaction changes

--- client/src/scenes/newTransaction/newtransaction2.jsx
+++ client/src/scenes/newTransaction/newtransaction2.jsx
@@ -18,14 +18,17 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 
 export default function ProductForm() {
-  const [products, setProducts] = useState([
-    { id: 1, name: 'Product 1' },
-    { id: 2, name: 'Product 2' },
-    { id: 3, name: 'Product 3' },
-  ]);
-  const [selectedProduct, setSelectedProduct] = useState('');
-  const [productQuantity, setProductQuantity] = useState('');
+  const [products, setProducts] = useState([    { id: 1, name: 'Product 1' },    { id: 2, name: 'Product 2' },    { id: 3, name: 'Product 3' },  ]);
+  const [selectedProducts, setSelectedProducts] = useState(['']);
+  const [productQuantities, setProductQuantities] = useState(['']);
+  const [numOfProducts, setNumOfProducts] = useState(1);
   const theme = useTheme();
+
+  const addProduct = () => {
+    setSelectedProducts([...selectedProducts, '']);
+    setProductQuantities([...productQuantities, '']);
+    setNumOfProducts(numOfProducts + 1);
+  };
 
   return (
     <Container component="main" maxWidth="md">   
@@ -41,40 +44,49 @@ export default function ProductForm() {
         <Typography variant="h2" component="div" color="secondary" sx={{mb: 3}}>
           New Transaction
         </Typography>
-        <Typography variant="h6" gutterBottom>
-        Product
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Select
-            required
-            id="product-select"
-            value={selectedProduct}
-            onChange={(e) => setSelectedProduct(e.target.value)}
-            fullWidth
-            variant="standard"
-          >
-            {products.map((product) => (
-              <MenuItem key={product.id} value={product.name}>
-                {product.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="product-quantity"
-            label="Product Quantity"
-            value={productQuantity}
-            onChange={(e) => setProductQuantity(e.target.value)}
-            fullWidth
-            autoComplete="off"
-            variant="standard"
-          />
-        </Grid>
-      </Grid>
-      </Box>
-    </Container>
-  )
-  }
+        {[...Array(numOfProducts)].map((_, i) => (
+          <>
+            <Typography variant="h6" gutterBottom>
+              Product {i + 1}
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Select
+                  required
+                  id={`product-select-${i}`}
+                  value={selectedProducts[i]}
+                  onChange={(e) => setSelectedProducts(selectedProducts.map((selected, j) => j === i ? e.target.value : selected))}
+                  fullWidth
+                  variant="standard"
+                >
+                  {products.map((product) => (
+                    <MenuItem key={product.id} value={product.name}>
+                      {product.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  id={`product-quantity-${i}`}
+                  label="Product Quantity"value={productQuantities[i]}
+                  onChange={(e) => setProductQuantities(productQuantities.map((quantity, j) => j === i ? e.target.value : quantity))}
+                  fullWidth
+                  variant="standard"
+                  type="number"
+                  inputProps={{min: 0}}
+                  />
+                  </Grid>
+                  </Grid>
+                  </>
+                  ))}
+                  <Box sx={{mt: 3}}>
+                  <Button variant="contained" color="primary" onClick={addProduct}>
+                  Add another product
+                  </Button>
+                  </Box>
+                  </Box>
+                  </Container>
+                  );
+                  }
