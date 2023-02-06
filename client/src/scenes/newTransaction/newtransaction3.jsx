413b7a5526ecf85dcf93ebe5797875cbb0227bd4
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-02-06 11:02:23 +0800

    transaction create button not yet working

--- client/src/scenes/newTransaction/newtransaction3.jsx
+++ client/src/scenes/newTransaction/newtransaction3.jsx
@@ -9,39 +9,30 @@ import {
   Button,
 } from "@mui/material";
 
-const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99',
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45',
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51',
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11',
-  },
-  { name: 'Shipping', desc: '', price: 'Free' },
-];
+import { gql, useMutation } from '@apollo/client'
+
 
 export default function Review() {
+
+let userArray = []
+let productArray = []
 
 let productReview = localStorage.getItem('savedItems');
 productReview = JSON.parse(productReview);
 console.log(productReview)
 
+const purple = localStorage.getItem('CustomerID')
+console.log('userId is: ', purple)
+// userArray.push(purple)
+// console.log('ARRAY: ', userArray)
+
 const prices = productReview.map(product => parseFloat(product.price.replace(',', '')));
 console.log(prices)
 
+const productIds = productReview.map(product => product.id);
+console.log('SHOWING:', productIds);
+productArray.push(productIds)
+console.log('PRODUCTS: ', productArray)
 
 
 let quantityReview = localStorage.getItem('savedQuantity');
@@ -59,8 +50,24 @@ function multiplyAndSumArrays(arr1, arr2) {
 
 const total = multiplyAndSumArrays(quantityReview, prices);
 
+const [createMutation] = useMutation(CREATE_TRANSACTION_MUTATION);
+const handleSubmit = evt => {
+  evt.preventDefault()
+  createMutation({
+    variables: {
+      userId: purple
+    }
+  }).then(res => {
+    console.log('Mutation response: ', res);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  console.log('SUBMITTED SUCCESSFULLY')
+}
   return (
     <React.Fragment>
+      <form onSubmit={evt => handleSubmit(evt)}>
       <Box>
       <Typography variant="h6" gutterBottom>
         Order summary
@@ -80,10 +87,22 @@ const total = multiplyAndSumArrays(quantityReview, prices);
           </Typography>
         </ListItem>
       </List>
-      <Button variant="contained" color="primary">
+      <Button type='submit' variant="contained" color="primary" onClick={evt => handleSubmit(evt)} >
             Place Order
       </Button>
       </Box>
+      </form>
     </React.Fragment>
   );
 }
+
+const CREATE_TRANSACTION_MUTATION = gql`
+  mutation Mutation($userIds: [ID!]) {
+    createTransaction(userIds: $userIds) {
+      id
+      userId {
+        name
+      }
+    }
+  }
+`
